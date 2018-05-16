9535d34a78686f143764c9a1e9b70400a19d69b6
Zhong Qiu <2609611@qq.com>
2018-05-16 10:14:43 +0800

    sign

---
+++ signtx.sh
@@ -0,0 +1,4 @@
+./ontology account add -d -password 123456 -label from
+./ontology account add -d -password 000000 -label to
+./ontology asset sign --from TA8krHYj4pqzgw3WhGSHMFW9XFGgxDus9Q --to TA5Z62x6tyxvaXc37yQguFDrkpp7eT7TeW --amount 6 -password 123456
+
